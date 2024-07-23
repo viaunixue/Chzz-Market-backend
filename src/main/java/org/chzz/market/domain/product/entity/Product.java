318cb0b59700c0bf206910a5c3f6af3c66185d4c
JongHyunJung <jjh19960115@gmail.com>
2024-07-24 08:22:31 +0900

    feat : Product Entity 내 Category Enum 내용 추가 (#30)

--- src/main/java/org/chzz/market/domain/product/entity/Product.java
+++ src/main/java/org/chzz/market/domain/product/entity/Product.java
@@ -55,5 +55,23 @@ public class Product extends BaseTimeEntity {
     private List<Like> likes = new ArrayList<>();
 
     public enum Category {
+        ELECTRONICS("전자기기"),
+        HOME_APPLIANCES("가전제품"),
+        FASHION_AND_CLOTHING("패션 및 의류"),
+        FURNITURE_AND_INTERIOR("가구 및 인테리어"),
+        BOOKS_AND_MEDIA("도서 및 미디어"),
+        SPORTS_AND_LEISURE("스포츠 및 레저"),
+        TOYS_AND_HOBBIES("장난감 및 취미"),
+        OTHER("기타");
+
+        private final String displayName;
+
+        Category(String displayName) {
+            this.displayName = displayName;
+        }
+
+        public String getDisplayName() {
+            return displayName;
+        }
     }
 }
