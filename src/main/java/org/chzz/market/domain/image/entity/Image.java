efa451b35deeef26a84e2e4e71e9935c72957621
JongHyunJung <jjh19960115@gmail.com>
2024-07-24 15:21:10 +0900

    refactor : Image Entity @Entity 어노테이션 추가 (#30)

--- src/main/java/org/chzz/market/domain/image/entity/Image.java
+++ src/main/java/org/chzz/market/domain/image/entity/Image.java
@@ -1,12 +1,6 @@
 package org.chzz.market.domain.image.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,6 +11,7 @@ import org.chzz.market.domain.product.entity.Product;
 
 @Getter
 @Builder
+@Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class Image extends BaseTimeEntity {
