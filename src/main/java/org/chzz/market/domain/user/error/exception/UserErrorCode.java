e31255565fcb5c19196ef363920f594de8a9ddfd
JongHyunJung <jjh19960115@gmail.com>
2024-07-31 03:59:26 +0900

    feat: 유저 비즈니스 에러코드 추가 - 유저를 찾을 수 없을 때 발생하는 에러 코드 (#30)

---
+++ src/main/java/org/chzz/market/domain/user/error/exception/UserErrorCode.java
@@ -0,0 +1,20 @@
+package org.chzz.market.domain.user.error.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.chzz.market.common.error.ErrorCode;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum UserErrorCode implements ErrorCode {
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    @Override
+    public String getCode() {
+        return "";
+    }
+}
