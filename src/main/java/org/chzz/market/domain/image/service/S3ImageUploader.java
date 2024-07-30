d84a3c32425b0e536c8e57b897c764b8813eb15c
JongHyunJung <jjh19960115@gmail.com>
2024-07-31 04:01:30 +0900

    feat: 이미지 저장 시 에러코드 반영 (#30)

--- src/main/java/org/chzz/market/domain/image/service/S3ImageUploader.java
+++ src/main/java/org/chzz/market/domain/image/service/S3ImageUploader.java
@@ -2,17 +2,14 @@ package org.chzz.market.domain.image.service;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
-import org.chzz.market.common.error.GlobalErrorCode;
-import org.chzz.market.common.error.exception.ImageUploadException;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.chzz.market.domain.image.error.ImageErrorCode;
+import org.chzz.market.domain.image.error.exception.ImageException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -34,7 +31,7 @@ public class S3ImageUploader implements ImageUploader {
 
             return "/" + fileName; // CDN 경로 생성 (전체 URL 아닌 경로만)
         } catch (IOException e) {
-            throw new ImageUploadException(GlobalErrorCode.IMAGE_UPLOAD_FAILED);
+            throw new ImageException(ImageErrorCode.IMAGE_UPLOAD_FAILED);
         }
     }
 }
