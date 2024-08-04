e30f4b9a48a57e4168fe2fad79e95c1585c05404
JongHyunJung <jjh19960115@gmail.com>
2024-08-04 23:43:12 +0900

    feat: 이미지 비즈니스 에러코드 추가 - 이미지 저장을 실패할 경우 발생하는 에러코드 (#35)

--- src/main/java/org/chzz/market/domain/image/error/ImageErrorCode.java
+++ src/main/java/org/chzz/market/domain/image/error/ImageErrorCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ImageErrorCode implements ErrorCode {
     IMAGE_UPLOAD_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "이미지 업로드를 실패했습니다."),
-    IMAGE_DELETE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "이미지 삭제를 실패했습니다. ");
+    IMAGE_DELETE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "이미지 삭제를 실패했습니다. "),
+    IMAGE_SAVE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "이미지 저장을 실패했습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
