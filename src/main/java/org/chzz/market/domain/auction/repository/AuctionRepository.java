a4ec17f499a7fc867f8d57bfd7baae2e2703b815
JongHyunJung <jjh19960115@gmail.com>
2024-08-04 17:09:41 +0900

    feat: findByProduct 메소드 추가 (#35)

--- src/main/java/org/chzz/market/domain/auction/repository/AuctionRepository.java
+++ src/main/java/org/chzz/market/domain/auction/repository/AuctionRepository.java
@@ -1,8 +1,11 @@
 package org.chzz.market.domain.auction.repository;
 
 import org.chzz.market.domain.auction.entity.Auction;
+import org.chzz.market.domain.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AuctionRepository extends JpaRepository<Auction, Long>, AuctionRepositoryCustom {
+import java.util.Optional;
 
+public interface AuctionRepository extends JpaRepository<Auction, Long>, AuctionRepositoryCustom {
+    Optional<Auction> findByProduct(Product product);
 }
